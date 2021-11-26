06eaf288c15b90cef665fc43cc18dc2571322666
kerimartin <keri.martin59@gmail.com>
2021-11-26 20:52:34 +0200

    code despaghettified and fixed some layout problems

--- app/src/main/java/com/example/secondproject/IBaseGpsListener.java
+++ app/src/main/java/com/example/secondproject/IBaseGpsListener.java
@@ -4,24 +4,30 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
 
-public class IBaseGpsListener implements LocationListener {
+
+public class IBaseGpsListener implements LocationListener
+{
     @Override
-    public void onLocationChanged(Location location) {
+    public void onLocationChanged(Location location)
+    {
 
     }
 
     @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
+    public void onStatusChanged(String provider, int status, Bundle extras)
+    {
 
     }
 
     @Override
-    public void onProviderEnabled(String provider) {
+    public void onProviderEnabled(String provider)
+    {
 
     }
 
     @Override
-    public void onProviderDisabled(String provider) {
+    public void onProviderDisabled(String provider)
+    {
 
     }
 }
--- app/src/main/java/com/example/secondproject/MainActivity.java
+++ app/src/main/java/com/example/secondproject/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.secondproject;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SwitchCompat;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -12,7 +11,6 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.util.Formatter;
@@ -20,68 +18,78 @@ import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
-
     TextView textViewSpeed;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         textViewSpeed = findViewById(R.id.textViewSpeed);
 
         //check for gps permission
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
             requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},100);
-        }else{
-            //start the program if the permission is granted
+        }
+        else
+        {
             doStuff();
         }
     }
 
     @Override
-    public void onLocationChanged(Location location) {
-        if(location!=null){
+    public void onLocationChanged(Location location)
+    {
+        if(location != null){
             CLocation myLocation = new CLocation(location);
             this.updateSpeed(myLocation);
             this.updateLocation(myLocation);
         }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras)
+    {
 
     }
 
     @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
+    public void onProviderEnabled(String provider)
+    {
 
     }
 
     @Override
-    public void onProviderEnabled(String provider) {
+    public void onProviderDisabled(String provider)
+    {
 
     }
 
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
     @SuppressLint("MissingPermission")
-    private void doStuff() {
+    private void doStuff()
+    {
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        if(locationManager != null){
+        if(locationManager != null)
+        {
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,this);
         }
         Toast.makeText(this,"Waiting GPS Connection!", Toast.LENGTH_SHORT).show();
     }
 
-    private void updateSpeed(CLocation location){
+    private void updateSpeed(CLocation location)
+    {
         float nCurrentSpeed =0;
-        if(location!=null){
+        if(location!=null)
+        {
             nCurrentSpeed = location.getSpeed();
         }
+
         Formatter fmt = new Formatter(new StringBuilder());
         fmt.format(Locale.US,"%5.1f",nCurrentSpeed);
         String strCurrentSpeed = fmt.toString();
         strCurrentSpeed =strCurrentSpeed.replace(" ","0");
-
 
         textViewSpeed.setText(strCurrentSpeed+" km/h");
     }
@@ -91,12 +99,16 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == 100) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
                 this.doStuff();
-            } else {
+            }
+            else
+            {
                 finish();
             }
         }
